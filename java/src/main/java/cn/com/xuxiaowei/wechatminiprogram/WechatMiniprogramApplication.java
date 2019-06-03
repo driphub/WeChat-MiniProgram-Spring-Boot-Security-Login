4770fc2df493af0fa8715a11970d0b57ec8e96fb
徐晓伟 <xuxiaowei@live.hk>
2019-06-03 11:29:40 +0800

    注释

--- java/src/main/java/cn/com/xuxiaowei/wechatminiprogram/WechatMiniprogramApplication.java
+++ java/src/main/java/cn/com/xuxiaowei/wechatminiprogram/WechatMiniprogramApplication.java
@@ -3,6 +3,11 @@ package cn.com.xuxiaowei.wechatminiprogram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ */
 @SpringBootApplication
 public class WechatMiniprogramApplication {
 
