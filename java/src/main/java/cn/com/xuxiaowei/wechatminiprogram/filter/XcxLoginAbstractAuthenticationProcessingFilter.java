bf855c3d1c895b65f8c2d8e52fe621efe87e8d62
徐晓伟 <xuxiaowei@live.hk>
2019-06-03 12:18:24 +0800

    小程序 登录 认证处理过滤器

---
+++ java/src/main/java/cn/com/xuxiaowei/wechatminiprogram/filter/XcxLoginAbstractAuthenticationProcessingFilter.java
@@ -0,0 +1,149 @@
+package cn.com.xuxiaowei.wechatminiprogram.filter;
+
+import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import cn.binarywang.wx.miniapp.config.WxMaInMemoryConfig;
+import cn.com.xuxiaowei.wechatminiprogram.propertie.WxMaProperties;
+import cn.com.xuxiaowei.wechatminiprogram.util.response.ResponseUtils;
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 小程序 登录 认证处理过滤器
+ * <p>
+ * 1、小程序登录：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/login.html
+ * 2、UnionID 机制说明：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/union-id.html
+ * 3、授权：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/authorize.html
+ * 4、服务端获取开放数据：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/signature.html
+ * 5、获取手机号：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/getPhoneNumber.html
+ *
+ * @author xuxiaowei
+ */
+@Slf4j
+public class XcxLoginAbstractAuthenticationProcessingFilter extends AbstractAuthenticationProcessingFilter {
+
+    @Autowired
+    private WxMaProperties wxMaProperties;
+
+    /**
+     * 基于内存的微信配置provider，在实际生产环境中应该将这些配置持久化
+     */
+    private static WxMaInMemoryConfig wxMaInMemoryConfig = new WxMaInMemoryConfig();
+    private static final WxMaServiceImpl wxMaService = new WxMaServiceImpl();
+
+    public XcxLoginAbstractAuthenticationProcessingFilter(String defaultFilterProcessesUrl) {
+        super(defaultFilterProcessesUrl);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        // 创建 Session，用于响应 Cookie
+        HttpSession session = request.getSession();
+
+        // 响应数据
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
+        String code = request.getParameter("code");
+
+        if (StringUtils.isEmpty(code)) {
+            map.put("code", 1);
+            map.put("msg", "微信小程序登录受到攻击！");
+            data.put("details", "未收到 微信小程序登录 的 code！");
+            log.debug(map.toString());
+            ResponseUtils.response(response, map);
+            return null;
+        }
+
+        try {
+
+            wxMaInMemoryConfig.setAppid(wxMaProperties.getAppid());
+            wxMaInMemoryConfig.setSecret(wxMaProperties.getSecret());
+            wxMaService.setWxMaConfig(wxMaInMemoryConfig);
+
+            // 微信小程序 登录 URL（使用 code 获取 OpenID、UnionID）
+            // 登录凭证校验。通过 wx.login（https://developers.weixin.qq.com/miniprogram/dev/api/open-api/login/wx.login.html） 接口获得临时登录凭证 code 后，
+            // 传到开发者服务器调用此接口完成登录流程。更多使用方法详见 小程序登录（https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/login.html）。
+            // 官方文档：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html
+            // 请求接口：https://api.weixin.qq.com/sns/jscode2session
+            // 接口示例：https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+            WxMaJscode2SessionResult wxMaJscode2SessionResult = wxMaService.jsCode2SessionInfo(code);
+
+
+            log.debug("");
+            log.debug("用户登录信息：" + wxMaJscode2SessionResult);
+            log.debug("");
+
+            // 普通用户的标识，对当前开发者帐号唯一
+            String openid = wxMaJscode2SessionResult.getOpenid();
+            // 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+            String unionid = wxMaJscode2SessionResult.getUnionid();
+            String sessionKey = wxMaJscode2SessionResult.getSessionKey();
+
+
+            //////////////////  ////////////////////
+
+
+            List<GrantedAuthority> authorities = new ArrayList<>();
+
+            // 给一个权限
+            authorities.add(new SimpleGrantedAuthority("ROLE_WECHATXCX"));
+
+            // 注意此时的类型 org.springframework.security.core.userdetails.User
+            org.springframework.security.core.userdetails.User user = new org.springframework.security.core.userdetails.User(openid, sessionKey, authorities);
+
+            WebAuthenticationDetails details = null;
+
+            SecurityContext context = SecurityContextHolder.getContext();
+            Authentication authentication = context.getAuthentication();
+
+            Object detailsObj = authentication.getDetails();
+
+            if (detailsObj instanceof WebAuthenticationDetails) {
+                details = (WebAuthenticationDetails) detailsObj;
+            }
+
+            // principal    用户 org.springframework.security.core.userdetails.User
+            // credentials  自定义信息、如：WebAuthenticationDetails
+            // authorities  权限
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(user, details, authorities);
+
+            map.put("code", 0);
+            map.put("msg", "微信小程序登录成功");
+            log.debug(map.toString());
+            ResponseUtils.response(response, map);
+
+            // 返回验证结果
+            return this.getAuthenticationManager().authenticate(authRequest);
+
+        } catch (WxErrorException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
