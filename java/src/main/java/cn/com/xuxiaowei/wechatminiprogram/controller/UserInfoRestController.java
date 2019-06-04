fef29798e5865e4d3bf324e8eb49394cedb2ea2a
徐晓伟 <xuxiaowei@live.hk>
2019-06-04 09:30:10 +0800

    优化逻辑、获取手机号

--- java/src/main/java/cn/com/xuxiaowei/wechatminiprogram/controller/UserInfoRestController.java
+++ java/src/main/java/cn/com/xuxiaowei/wechatminiprogram/controller/UserInfoRestController.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.wechatminiprogram.controller;
 
 import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+import cn.binarywang.wx.miniapp.bean.WxMaPhoneNumberInfo;
 import cn.binarywang.wx.miniapp.bean.WxMaUserInfo;
 import cn.binarywang.wx.miniapp.config.WxMaInMemoryConfig;
 import cn.com.xuxiaowei.wechatminiprogram.propertie.WxMaProperties;
@@ -29,10 +30,15 @@ import java.util.Map;
 public class UserInfoRestController {
 
     private final WxMaProperties wxMaProperties;
+    private final String appid;
+    private final String secret;
 
     @Autowired
     public UserInfoRestController(WxMaProperties wxMaProperties) {
         this.wxMaProperties = wxMaProperties;
+        appid = wxMaProperties.getAppid();
+        secret = wxMaProperties.getSecret();
+        wxMaService.setWxMaConfig(wxMaInMemoryConfig);
     }
 
     /**
@@ -66,6 +72,10 @@ public class UserInfoRestController {
                                          String avatarUrl,
                                          String language) {
 
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
         log.debug("前台发送的用户信息（不需要解密）为：");
         log.debug("昵称：" + nickName);
         log.debug("性别：" + gender);
@@ -77,13 +87,76 @@ public class UserInfoRestController {
 
         ////////////////////////////// 下面是通过解密得到用户数据 //////////////////////////////
 
+        // 会话密钥
+        String sessionKey = getUserDetailsPassword();
+
+        if (sessionKey != null) {
+
+            // 解密用户信息
+            WxMaUserInfo wxMaUserInfo = wxMaService.getUserService().getUserInfo(sessionKey, encryptedData, iv);
+
+            map.put("code", 0);
+            map.put("msg", "更新用户详细信息成功");
+            data.put("wxMaUserInfo", wxMaUserInfo);
+
+        } else {
+            map.put("code", 1);
+            map.put("msg", "更新用户详细信息失败！");
+            data.put("details", "Security 中未找到用户！");
+        }
+
+        return map;
+    }
+
+
+    /**
+     * 获取用户手机号
+     * <p>
+     * 此方法在用户点击同意获取手机号
+     *
+     * @param encryptedData 包括敏感数据在内的完整用户信息的加密数据，详细见 加密数据解密算法
+     *                      https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/signature.html#%E5%8A%A0%E5%AF%86%E6%95%B0%E6%8D%AE%E8%A7%A3%E5%AF%86%E7%AE%97%E6%B3%95
+     * @param iv            加密算法的初始向量，详细见 加密数据解密算法
+     *                      https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/signature.html#%E5%8A%A0%E5%AF%86%E6%95%B0%E6%8D%AE%E8%A7%A3%E5%AF%86%E7%AE%97%E6%B3%95
+     */
+    @RequestMapping("/getPhoneNumber.do")
+    public Map<String, Object> getPhoneNumber(HttpServletRequest request, HttpServletResponse response,
+                                              String encryptedData, String iv) {
+
         Map<String, Object> map = new HashMap<>(4);
         Map<String, Object> data = new HashMap<>(4);
         map.put("data", data);
 
-        wxMaInMemoryConfig.setAppid(wxMaProperties.getAppid());
-        wxMaInMemoryConfig.setSecret(wxMaProperties.getSecret());
-        wxMaService.setWxMaConfig(wxMaInMemoryConfig);
+        ////////////////////////////// 下面是通过解密得到用户数据 //////////////////////////////
+
+        // 会话密钥
+        String sessionKey = getUserDetailsPassword();
+
+        // 用户类型转换
+        if (sessionKey != null) {
+
+            // 解密手机号
+            WxMaPhoneNumberInfo wxMaPhoneNumberInfo = wxMaService.getUserService().getPhoneNoInfo(sessionKey, encryptedData, iv);
+
+            map.put("code", 0);
+            map.put("msg", "获取用户手机号成功");
+            data.put("wxMaPhoneNumberInfo", wxMaPhoneNumberInfo);
+
+        } else {
+            map.put("code", 1);
+            map.put("msg", "获取用户手机号失败！");
+            data.put("details", "Security 中未找到用户！");
+        }
+
+        return map;
+    }
+
+    /**
+     * 在 Security 获取 微信小程序 会话密钥
+     * <p>
+     * 在小程序登录时放入的
+     */
+    private String getUserDetailsPassword() {
 
         // 获取 Security 中的授权信息
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -101,22 +174,12 @@ public class UserInfoRestController {
             // 方式二：<code>String openid = userDetails.getUsername();</code>
 
             // 会话密钥
-            String sessionKey = userDetails.getPassword();
-
-            // 解密用户信息
-            WxMaUserInfo wxMaUserInfo = wxMaService.getUserService().getUserInfo(sessionKey, encryptedData, iv);
-
-            map.put("code", 0);
-            map.put("msg", "更新用户详细信息成功");
-            data.put("wxMaUserInfo", wxMaUserInfo);
+            return userDetails.getPassword();
 
         } else {
-            map.put("code", 1);
-            map.put("msg", "更新用户详细信息失败！");
-            data.put("details", "Security 中未找到用户！");
+            return null;
         }
 
-        return map;
     }
 
 }
